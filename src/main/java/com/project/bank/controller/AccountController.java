22e5b62a5df90bd046843c1816a5a3848c78b13e
sy <20230601sy@gmail.com>
2023-11-09 14:56:43 +0900

    change return responseEntity w/ only status if it can @ accountController

--- src/main/java/com/project/bank/controller/AccountController.java
+++ src/main/java/com/project/bank/controller/AccountController.java
@@ -33,7 +33,7 @@ public class AccountController {
 	@GetMapping("/account/{id}")
 	public ResponseEntity<?> getAccount(Authentication authentication, @PathVariable int id) {
 		User loginUser = userService.getAuthUser(authentication.getName());
-		
+
 		Account account = accountService.getAccount(loginUser, id);
 		if(account == null)
 			return new ResponseEntity<>("계좌조회 실패", HttpStatus.BAD_REQUEST);
@@ -45,52 +45,55 @@ public class AccountController {
 	public ResponseEntity<?> insertAccount(Authentication authentication) {
 		User loginUser = userService.getAuthUser(authentication.getName());
 		int result = accountService.insertAccount(loginUser);
-		
-		// 지금은 이해를 돕기 위해 message를 넣지만 차후에는 status만 보내도록 변경해서 if문 분기 없앨 예정!
-		// return new ResponseEntity<>(HttpStatus.valueOf(result)); (수정할 거면 일단 동작하는지 확인부터 하고...)
-		
-		if(result == 400) 
-			return new ResponseEntity<>("계좌개설 제한", HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(HttpStatus.valueOf(result));
 
-		if(result == 500)
-			return new ResponseEntity<>("계좌개설 실패", HttpStatus.BAD_REQUEST);
-
-		return new ResponseEntity<>("계좌개설 완료", HttpStatus.OK);
+//		지금은 이해를 돕기 위해 message를 넣지만 차후에는 status만 보내도록 변경해서 if문 분기 없앨 예정!
+//		return new ResponseEntity<>(HttpStatus.valueOf(result)); (수정할 거면 일단 동작하는지 확인부터 하고...)
+//		if(result == 400) 
+//			return new ResponseEntity<>("계좌개설 제한", HttpStatus.BAD_REQUEST);
+//
+//		if(result == 500)
+//			return new ResponseEntity<>("계좌개설 실패", HttpStatus.BAD_REQUEST);
+//
+//		return new ResponseEntity<>("계좌개설 완료", HttpStatus.OK);
 	}
 	
 	@PostMapping("/account/{username}")
 	public ResponseEntity<?> insertAccountWithManager(@PathVariable String username) {
 		User user = userService.getAuthUser(username);
 		int result = accountService.insertAccount(user);
+		return new ResponseEntity<>(HttpStatus.valueOf(result));
 		
-		if(result == 400)
-			return new ResponseEntity<>("계좌개설 제한", HttpStatus.BAD_REQUEST);
-
-		if(result == 500)
-			return new ResponseEntity<>("계좌개설 실패", HttpStatus.BAD_REQUEST);
-
-		return new ResponseEntity<>("계좌개설 완료", HttpStatus.OK);
+//		if(result == 400)
+//			return new ResponseEntity<>("계좌개설 제한", HttpStatus.BAD_REQUEST);
+//
+//		if(result == 500)
+//			return new ResponseEntity<>("계좌개설 실패", HttpStatus.BAD_REQUEST);
+//
+//		return new ResponseEntity<>("계좌개설 완료", HttpStatus.OK);
 	}
 
 	@DeleteMapping("/account/{id}")
 	public ResponseEntity<?> deleteAccount(Authentication authentication, @PathVariable int id) {
 		User loginUser = userService.getAuthUser(authentication.getName());
 		int result = accountService.deleteAccount(loginUser, id);
-		
-		if(result == 400)
-			return new ResponseEntity<>("계좌해지 실패", HttpStatus.BAD_REQUEST);
-		
-		return new ResponseEntity<>("계좌해지 완료", HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.valueOf(result));
+
+//		if(result == 400)
+//			return new ResponseEntity<>("계좌해지 실패", HttpStatus.BAD_REQUEST);
+//		
+//		return new ResponseEntity<>("계좌해지 완료", HttpStatus.OK);
 	}
 
 	@DeleteMapping("/account/{id}/{username}")
 	public ResponseEntity<?> deleteAccountWithManager(@PathVariable int id, @PathVariable String username) {
 		User user = userService.getAuthUser(username);
 		int result = accountService.deleteAccount(user, id);
+		return new ResponseEntity<>(HttpStatus.valueOf(result));
 
-		if(result == 400)
-			return new ResponseEntity<>("계좌해지 실패", HttpStatus.BAD_REQUEST);
-		
-		return new ResponseEntity<>("계좌해지 완료", HttpStatus.OK);
+//		if(result == 400)
+//			return new ResponseEntity<>("계좌해지 실패", HttpStatus.BAD_REQUEST);
+//		
+//		return new ResponseEntity<>("계좌해지 완료", HttpStatus.OK);
 	}
 }
